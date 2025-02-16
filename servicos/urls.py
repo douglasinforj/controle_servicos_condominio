93b3a38fb5df153767666ad877f3e2af8721f1f1
douglasinforj <douglasitpro@gmail.com>
2025-02-16 12:49:31 -0300

    Ajuste de escrita

--- servicos/urls.py
+++ servicos/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CondominoViewSet, EmpresaParceiraViewSet, SolicitacaoServicoSerializer, ComentarioViewSet
+from .views import CondominoViewSet, EmpresaParceiraViewSet, SolicitacaoServicoViewSet, ComentarioViewSet
 
 router = DefaultRouter()
 router.register(r'condominos', CondominoViewSet)
 router.register(r'empresas', EmpresaParceiraViewSet)
-router.register(r'solicitacoes', SolicitacaoServicoSerializer)
+router.register(r'solicitacoes', SolicitacaoServicoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)), 
+    path('', include(router.urls)),
 ]
