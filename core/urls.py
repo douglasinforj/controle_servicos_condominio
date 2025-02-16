3c97f49b0f81cf43dc82ba6ac9de52bf30ca0fd0
douglasinforj <douglasitpro@gmail.com>
2025-02-16 16:42:04 -0300

    adicionando rota de autenticação

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+#autenticação
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('servicos.urls')), 
+
+    path('api/auth', obtain_auth_token, name='api_token_auth'),
 ]
 
 if settings.DEBUG:
