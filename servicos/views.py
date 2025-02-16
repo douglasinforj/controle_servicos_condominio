05124f9d1168c9d1e9cb3f2113e338d170a6928f
douglasinforj <douglasitpro@gmail.com>
2025-02-16 12:20:32 -0300

    Adicionando Views para as rotas API

--- servicos/views.py
+++ servicos/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Condomino, EmpresaParceira, SolicitacaoServico, Comentario
+from .serializers import CondominoSerializer, EmpresaParceiraSerializer, SolicitacaoServicoSerializer, ComentarioSerializer
 
-# Create your views here.
+class CondôminoViewSet(viewsets.ModelViewSet):
+    queryset = Condomino.objects.all()
+    serializer_class = CondominoSerializer
+
+class EmpresaParceiraViewSet(viewsets.ModelViewSet):
+    queryset = EmpresaParceira.objects.all()
+    serializer_class = EmpresaParceiraSerializer
+
+class SolicitacaoServicoViewSet(viewsets.ModelViewSet):
+    queryset = SolicitacaoServico.objects.all()
+    serializer_class = SolicitacaoServicoSerializer
+
+class ComentarioViewSet(viewsets.ModelViewSet):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
